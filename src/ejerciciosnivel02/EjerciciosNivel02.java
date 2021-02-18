01ca4a3de820ef4d7c159183b655df992a7aebd6
Paco2 <Paco2@LAPTOP-JT3V7053>
2021-02-18 12:57:47 +0100

    Ejercicio findDuplicate con el método Floyd

--- src/ejerciciosnivel02/EjerciciosNivel02.java
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -34,9 +34,7 @@ public class EjerciciosNivel02 {
         }  
         return true;
     }
-    
-    
-    
+
     
     public int costeErroresADN(String uno, String dos){
         int contador = 0;
@@ -67,7 +65,6 @@ public class EjerciciosNivel02 {
         
         return contador;
     }
-    
     public int costeErroresADN_Jorge(String uno, String dos){
         int coste = 0;
         
@@ -126,6 +123,36 @@ public class EjerciciosNivel02 {
     }
     
     /**
+     * Encuentra el número que está duplicado en una lista de ints
+     * usando el algoritmo de Floyd
+     * @param listaNumeros
+     * @return 
+     */
+    // 0 1 2 3 4
+    //[1,3,2,4,4]
+    public int findDuplicate(int[] listaNumeros){
+        int tortuga = listaNumeros[0];
+        int liebre = listaNumeros[0];
+        boolean iguales = false;
+        while(!iguales){
+            tortuga = listaNumeros[tortuga];
+            liebre = listaNumeros[listaNumeros[liebre]];
+            if(tortuga == liebre){
+                iguales = true;
+            }
+        }
+        int auxiliar1 = listaNumeros[0];
+        int auxiliar2 = tortuga;
+        while(auxiliar1 != auxiliar2){
+            auxiliar1 = listaNumeros[auxiliar1];
+            auxiliar2 = listaNumeros[auxiliar2];
+        }
+        return auxiliar1;
+    }
+    
+    
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
@@ -144,7 +171,7 @@ public class EjerciciosNivel02 {
         
         System.out.println(ejercicio.esEscaleraDePalabras(listaPalabras)); */
  
-        System.out.println(ejercicio.costeErroresADN_Jorge("ACGT", "TGCA"));
+        /*System.out.println(ejercicio.costeErroresADN_Jorge("ACGT", "TGCA"));
         System.out.println("");
         System.out.println(ejercicio.costeErroresADN_Jorge("A-C-G-T-ACGT", "TTGGCCAATGCA"));
         System.out.println("");
@@ -160,7 +187,11 @@ public class EjerciciosNivel02 {
         System.out.println("");
         System.out.println(ejercicio.costeErroresADN_Jorge("GGGA-GAATATCTGGACT", "CCCTACTTA-AGACCGGT"));
         System.out.println("");
-        System.out.println(ejercicio.costeErroresADN_Jorge("-ATG", "-TAC"));
+        System.out.println(ejercicio.costeErroresADN_Jorge("-ATG", "-TAC"));*/
+        
+        System.out.println(ejercicio.findDuplicate(new int[]{3,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[]{4,2,3,4,1}));
+        System.out.println(ejercicio.findDuplicate(new int[]{1,1,3,4,1}));
     }
     
 }
